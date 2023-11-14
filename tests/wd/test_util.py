68f05f867ade49ad0d73fccadd00bb7127470382
Antti Kuparinen <akuparinen@iki.fi>
2023-11-14 09:29:08 +0200

    Added StrInLanguages class and tests

--- tests/wd/test_util.py
+++ tests/wd/test_util.py
@@ -1,3 +1,5 @@
+import pytest
+
 from wd.util import *
 
 def test_spaced():
@@ -13,7 +15,8 @@ def test_spaced():
 def test_build_tooltip():
     assert build_tooltip('cat', None, None, None) == 'cat'
     assert build_tooltip('cat', None, None, 'an animal') == 'cat: an animal'
-    assert build_tooltip('cat', ['domestic cat'], None, 'an animal') == 'cat (domestic cat): an animal'
+    assert build_tooltip('cat', ['domestic cat'], None, 'an animal') \
+        == 'cat (domestic cat): an animal'
 
     assert build_tooltip('kissa', None, 'cat', None) == 'kissa [= cat]'
     assert build_tooltip('kissa', None, 'cat', 'an animal') == 'kissa [= cat]: an animal'
@@ -23,3 +26,32 @@ def test_build_tooltip():
 
     assert build_tooltip('kissa', ['kotikissa', 'kesykissa'], 'cat', 'an animal') \
         == 'kissa (kotikissa, kesykissa) [= cat]: an animal'
+
+
+
+
+
+
+def test_StrsInLanguage():
+    assert StrInLanguages({ 'en': 'cat', 'fi': 'kissa' }).get('fi') \
+        == StrInLanguage('kissa', lang='fi')
+
+    assert StrInLanguages({ 'en': 'cat', 'fi': 'kissa' }).get('fi', 'en') \
+        == StrInLanguage('kissa', lang='fi')
+
+    assert StrInLanguages({ 'en': 'cat', 'fi': 'kissa' }).get('es', 'en') \
+        == StrInLanguage('cat', lang='en')
+
+    assert StrInLanguages({ 'en': ['cat', 'domestic cat'], 'fi': ['kissa', 'kotikissa'] }).get('fi') \
+        == [StrInLanguage('kissa', lang='fi'), StrInLanguage('kotikissa', lang='fi')]
+
+    with pytest.raises(NotImplementedError):
+        StrInLanguages({ 'en': 3, 'fi': 4 }).get('fi')
+
+    with pytest.raises(Exception):
+        StrInLanguages({ 'en': [2, 3], 'fi': [4, 5] }).get('fi')
+
+
+    x = StrInLanguages({ 'en': 'cat', 'fi': 'kissa' })
+    x.add(StrInLanguage('gato', lang='es'))
+    assert x == StrInLanguages({ 'en': 'cat', 'fi': 'kissa', 'es': 'gato' })
