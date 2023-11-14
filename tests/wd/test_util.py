8d41c20331dff75b63b869e729a7f8a4326be32e
Antti Kuparinen <akuparinen@iki.fi>
2023-11-14 11:13:45 +0200

    Added function bind_sparql_query and tests

--- tests/wd/test_util.py
+++ tests/wd/test_util.py
@@ -55,3 +55,27 @@ def test_StrsInLanguage():
     x = StrInLanguages({ 'en': 'cat', 'fi': 'kissa' })
     x.add(StrInLanguage('gato', lang='es'))
     assert x == StrInLanguages({ 'en': 'cat', 'fi': 'kissa', 'es': 'gato' })
+
+
+
+def test_bind_sparql_query():
+    assert bind_sparql_query("abc") == "abc"
+
+    assert bind_sparql_query("{{x}}", x=1) == "1"
+    assert bind_sparql_query("{{x}}", x=1.2) == "1.2"
+    assert bind_sparql_query("{{x}}", x='word') == '"word"'
+    assert bind_sparql_query("{{x}}", x=Identifier('fi')) == 'fi'
+
+    assert bind_sparql_query("{{x}} = {{x}}", x=1) == "1 = 1"
+    assert bind_sparql_query("{{x}} + {{y}} = {{z}}", x=1, y=2, z=3) == "1 + 2 = 3"
+
+
+
+    with pytest.raises(Exception):
+        bind_sparql_query("{{x}}", x='"cat"')
+
+    with pytest.raises(Exception):
+        bind_sparql_query("{{x}} {{y}}", x='1')
+
+    with pytest.raises(Exception):
+        bind_sparql_query("{{x}}", x='1', y="2")
--- wd/util.py
+++ wd/util.py
@@ -1,3 +1,4 @@
+import re
 import pprint
 
 
@@ -80,3 +81,52 @@ class StrInLanguages:
         self.__dict[lang] = item.text
 
         print(self.__dict)
+
+
+def check_for_invalid_values(params):
+    for value in params.values():
+        if type(value) == str and value.find('"') != -1:
+            raise Exception(f'Invalid parametre value: "{value}"')
+
+
+def check_for_extra_keys(query, params):
+    for key, value in params.items():
+        if query.find("{{" + key + "}}") == -1:
+            raise Exception(f"Parametre {key} not found in query")
+
+
+def check_for_missing_keys(query, params):
+    placeholder_regex = re.compile(r"\{\{([^\d\W]\w*)\}\}", re.UNICODE)
+
+    for name in re.findall(placeholder_regex, query):
+        if name not in params:
+            raise Exception(f"Placeholder {name} not given a value")
+
+
+class Identifier:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+def bind_sparql_query(query, **params):
+    check_for_invalid_values(params)
+    check_for_extra_keys(query, params)
+    check_for_missing_keys(query, params)
+
+    for key, value in params.items():
+        if query.find("{{" + key + "}}") == -1:
+            raise Exception(f"Parametre {key} not found in query")
+
+        if type(value) == str:
+            query = query.replace("{{" + key + "}}", f'"{value}"')
+        elif type(value) in [int, float]:
+            query = query.replace("{{" + key + "}}", str(value))
+        elif type(value) == Identifier:
+            query = query.replace("{{" + key + "}}", str(value))
+        else:
+            raise Exception(f'Invalid type: {str(value)}')
+
+    return query
