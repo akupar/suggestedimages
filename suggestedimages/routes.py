7fb3e0d27ecfc4c28ba4c72d22b3d9f6404c2ce9
Antti Kuparinen <akuparinen@iki.fi>
2023-11-14 20:54:07 +0200

    Added icons to open the wiktionary page in view and edit mode

--- suggestedimages/routes.py
+++ suggestedimages/routes.py
@@ -1,6 +1,7 @@
 from flask import (
     Blueprint, flash, redirect, render_template, request, url_for
 )
+import urllib
 
 import wd
 from wd.util import StrInLanguage
@@ -18,7 +19,13 @@ def index():
     locale = Locale(wikt) if wikt else DEFAULT_LOCALE
     lang = request.args.get('lang') or locale.language
     lang_str = StrInLanguage(title, lang=lang)
+    encoded_title = urllib.parse.quote(title.replace(' ', '_'), safe='/', encoding=None, errors=None)
+    edit_url = f"https://{wikt}.wiktionary.org/w/index.php?title={encoded_title}&action=edit" if wikt else None
+    view_url = f"https://{wikt}.wiktionary.org/wiki/{encoded_title}" if wikt else None
+
 
     return render_template('index.html',
                            results=wd.get_images_for_search(lang_str, locale),
+                           edit_url=edit_url,
+                           view_url=view_url,
                            locale=locale)
