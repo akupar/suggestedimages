4fe4ee38eccfb0634b6b99fa13c21056e0025ba4
Antti Kuparinen <akuparinen@iki.fi>
2023-11-19 14:34:26 +0200

    Added clarifying comments

--- suggestedimages/wikidata/api.py
+++ suggestedimages/wikidata/api.py
@@ -89,9 +89,11 @@ def yield_image_pages(generator: Iterator, searched: StrInLanguage, locale: Loca
             yield NoImage, entry_info
 
 
+        # Property P373 = Commons category
         if (prop := entry.claims.get('P373')) and len(prop) > 0:
             yield CommonsResult('Category:' + prop[0].getTarget(), 'category'), entry_info
 
+        # Property P935 = Commons gallery page
         if (prop := entry.claims.get('P935')) and len(prop) > 0:
             yield CommonsResult(prop[0].getTarget(), 'gallery'), entry_info
 
@@ -100,6 +102,8 @@ def get_images_for_word(searched: StrInLanguage, locale: Locale) -> list[tuple[R
     entries_generator = yield_label_or_alias_results(searched)
 
     result_tuples = list(yield_image_pages(entries_generator, searched, locale))
+
+    # Get ranking for results to sort them so that the best results come first.
     ranks = get_ranks_for_entries(result_tuples, searched)
 
     return sorted(
@@ -110,12 +114,18 @@ def get_images_for_word(searched: StrInLanguage, locale: Locale) -> list[tuple[R
 
 
 
-def get_ranks_for_entries(results: list[tuple[Result, WDEntry]], searched: StrInLanguage):
+def get_ranks_for_entries(results: list[tuple[Result, WDEntry]], searched: StrInLanguage) -> dict[str, tuple[bool, bool bool]]:
     entry_ranks = {}
 
+    # Since the results are sorted by the wikidata entry and there are multiple results for each entry,
+    # there is one ranking for each entry. We update the same entry for each image in that entry.
     for result_info, entry_info in results:
         if entry_info.id not in entry_ranks:
-            entry_ranks[entry_info.id] = (False, True, False)
+            entry_ranks[entry_info.id] = (
+                False, # Result matches exactly
+                True,  # There is at least one image, initalized to True, because there is a NoImages item if no images were found.
+                False  # There is a gallery or a category
+            )
 
         exact_case_match = (entry_info.label == searched or searched in entry_info.aliases)
         no_images = (result_info == NoImage)
@@ -129,6 +139,9 @@ def get_ranks_for_entries(results: list[tuple[Result, WDEntry]], searched: StrIn
         )
 
     return entry_ranks
+
+
+
 
 
 if __name__ == "__main__":
