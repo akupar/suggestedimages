8d534d81a494be9d94fe037a568731eb3cefaa23
Antti Kuparinen <akuparinen@iki.fi>
2023-11-19 18:13:48 +0200

    Added a new query to find entries with a given property having certain values

--- suggestedimages/wikidata/queries/__init__.py
+++ suggestedimages/wikidata/queries/__init__.py
@@ -1,14 +1,17 @@
+from typing import *
+
 from .util import bind_sparql_query, Identifier
 
-def label_or_alias_capitalized_or_not(text, language, limit=50):
+
+def label_or_alias_capitalized_or_not(text: str, language: str, limit=50):
     return bind_sparql_query('''
-SELECT distinct ?item ?itemLabel ?itemDescription WHERE{
+SELECT DISTINCT ?item WHERE{
   VALUES ?prefLabel {
     {{text}}@{{language}}
     {{text_capitalized}}@{{language}}
   }
 
-  ?item rdfs:label|skos:altLabel ?prefLabel
+  ?item rdfs:label|skos:altLabel ?prefLabel .
 }
 
 LIMIT {{limit}}
@@ -16,5 +19,25 @@ LIMIT {{limit}}
         text = text,
         text_capitalized = text.capitalize(),
         language = Identifier(language),
-        limit = 50
+        limit = limit
+    )
+
+
+
+def property_has_any_of_values(property: str, values: list[str], limit=50):
+    """Return items where property `property` has any of values `values`.
+    """
+    return bind_sparql_query('''
+SELECT DISTINCT ?item WHERE{
+  VALUES ?value {
+    {{values}}
+  }
+  ?item wdt:{{property}} ?value .
+}
+
+LIMIT {{limit}}
+''',
+        property = Identifier(property),
+        values = values,
+        limit = limit
     )
