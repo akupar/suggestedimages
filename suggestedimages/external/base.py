a509770731fcfe5e5d81827a22655f9d5f2afeba
Antti Kuparinen <akuparinen@iki.fi>
2023-11-20 12:22:56 +0200

    Added documentation about implementing new handlers.

--- suggestedimages/external/base.py
+++ suggestedimages/external/base.py
@@ -12,5 +12,11 @@ class ExternalResult:
 
 
 class ExternalApi:
+    """Base class for external api handlers.
+
+    To add a new handler, subclass this and implement `get` method and
+    Return `ExternalResult` with relevant information.
+    """
+
     def get(self, word: str) -> list[ExternalResult]:
         raise NotImplementedError
