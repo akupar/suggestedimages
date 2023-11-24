3a564ebb301e5f9c61c138e6d3f9f13a941fe9a0
Antti Kuparinen <akuparinen@iki.fi>
2023-11-24 12:20:22 +0200

    Added Spanish locale

---
+++ suggestedimages/locales/locales/es.py
@@ -0,0 +1,16 @@
+from ..cldr_names import LanguageNames
+
+texts = {
+    '[no label]': '[sin etiqueta]',
+    '(same)': '(mismo)',
+    'File': 'Archivo',
+    'thumb': 'thumb',
+    'Search': 'Buscar',
+    'Wiktionary': 'Wikcionario',
+    'Language': 'Idioma',
+    'Title': 'Entrada',
+    'Loading...': 'Cargando...',
+    'No results.': 'No hay resultados.',
+}
+
+language_names = LanguageNames('es')
