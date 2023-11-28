57e96984b39db27c7526c9a6964af29cf4e6504a
Antti Kuparinen <akuparinen@iki.fi>
2023-11-28 16:21:31 +0200

    Refactor for readability - extract code to descriptive functions - write out unneccessary abbreviations - add clarifying comments on title input focusing and gallery hiding

--- suggestedimages/static/index.js
+++ suggestedimages/static/index.js
@@ -1,34 +1,61 @@
-function updateLanguage(langElem) {
-    const code = langElem.value;
-    const option = document.querySelector('#languages').querySelector('option[value="' + code + '"]');
-    if ( option ) {
-        langElem.setCustomValidity("");
-        document.querySelector('#language-name').textContent = option.textContent.replace(/ \[.*\]/, '');
-    } else if ( code == "" ) {
-        langElem.setCustomValidity("");
-        document.querySelector('#language-name').textContent = "(same)";
+function setLanguageNameDisplay(text) {
+    document.querySelector('#language-name').textContent = text;
+}
 
+function extractLanguageNameFromOptionLabel(optionValue) {
+    // For example, 'Spanish [es]' –> 'Spanish'
+    return optionValue.replace(/ \[.*\]/, '');
+}
+
+function handleChangedLanguage(languageInput) {
+    const code = languageInput.value;
+    const option = document.querySelector('#languages').querySelector('option[value="' + code + '"]');
+
+    if ( option ) {
+        setLanguageNameDisplay(extractLanguageNameFromOptionLabel(otion.textContent));
+        languageInput.setCustomValidity("");
+    } else if ( code == "" ) {
+        setLanguageNameDisplay("(same)");
+        languageInput.setCustomValidity("");
     } else {
-        langElem.setCustomValidity("Invalid language code");
-        document.querySelector('#language-name').textContent = "";
+        setLanguageNameDisplay("");
+        languageInput.setCustomValidity("Invalid language code");
     }
 }
 
+function focusTitleInputAndSelectText() {
+    // The HTML autofocus attribute just focuses, but doesn't select the
+    // text, so we have to do it ourselves.
+    document.getElementById('title').select();
+}
+
+function titleInputIsEmpty() {
+    return (document.getElementById('title').value.trim() === "");
+}
+
+
+function showLoadingIndicator() {
+    document.querySelector('.gallery').style.display = 'block';
+}
+
+function hideGallery(isLoading) {
+    document.querySelector('.loading-message').style.display = 'none';
+}
+
+
 document.addEventListener("DOMContentLoaded", (event) => {
-    const textbox = document.getElementById('title');
-    if ( textbox.value.trim() !== "" ) {
-        document.getElementById('title').select();
+    if ( ! titleInputIsEmpty() ) {
+        focusTitleInputAndSelectText();
     }
-    updateLanguage(document.querySelector('#lang'));
+    handleChangedLanguage(document.querySelector('#lang'));
 });
 
-document.querySelector('#wikt').addEventListener("change", (event) => {
+document.forms[0].wikt.addEventListener("change", (event) => {
     document.forms[0].submit();
 });
 
-
 document.forms[0].addEventListener("submit", (event) => {
-    // Disable empty inputs, so it is not submitted as a parametre and clutter the url.
+    // Disable empty inputs, so they are not submitted as parametres and clutter the url.
     if ( document.forms[0].lang.value === "" ) {
         document.forms[0].lang.setAttribute("disabled", true);
     }
@@ -37,11 +64,9 @@ document.forms[0].addEventListener("submit", (event) => {
         document.forms[0].title.setAttribute("disabled", true);
     }
 
-    document.querySelector('.gallery').style.display = 'none';
-    document.querySelector('.loading-message').style.display = 'block';
+    showLoadingIndicator();
+    // Hide the gallery temporarily to make the changing of results more visible.
+    hideGallery();
 });
 
-
-
-document.querySelector('#lang')
-        .addEventListener('input', (event) => updateLanguage(event.target));
+document.forms[0].lang.addEventListener('input', (event) => handleChangedLanguage(event.target));
